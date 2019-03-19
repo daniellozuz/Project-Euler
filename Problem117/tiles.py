7efe87d22afdefd0f941ae20b8636b0da937945a
Daniel Zuziak <daniel.zuziak@gmail.com>
2019-03-19 12:47:12 +0100

    Refactored the solution

--- Problem117/tiles.py
+++ Problem117/tiles.py
@@ -5,12 +5,9 @@ from functools import lru_cache
 def solution(length):
     if length < 0:
         return 0
-    if length in {0, 1}:
+    if length == 0:
         return 1
-    return sum([2 * solution(length - 2),
-                2 * solution(length - 3),
-                2 * solution(length - 4),
-                solution(length - 5)])
+    return sum(solution(length - decrement - 1) for decrement in range(4))
 
 
 if __name__ == '__main__':
