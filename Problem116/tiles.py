ff4579d1a23fe35c4d5de81db369ddda9ace1102
Daniel Zuziak <daniel.zuziak@gmail.com>
2019-03-19 11:54:03 +0100

    Easy tiling problem

---
+++ Problem116/tiles.py
@@ -0,0 +1,15 @@
+import inspect
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def f(length, size):
+    return 1 + sum(f(shorter, size) for shorter in range(length - size + 1))
+
+
+def solution(length):
+    return f(length, 4) + f(length, 3) + f(length, 2) - 3
+
+
+if __name__ == '__main__':
+    print(solution(50))
